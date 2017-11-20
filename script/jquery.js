43928ad2924b3617f39bef5dff7874b9135a0c25
Jonathan Sebastian <jojosebseb@gmail.com>
2017-11-20 10:33:08 +0700

    v1

--- script/jquery.js
+++ script/jquery.js
@@ -12,17 +12,66 @@ $('#workSlider').slick({
     arrows: true,
     swipe: true,
     focusOnSelect: true,
+    infinite: true,
     autoplay: false,
     autoplaySpeed: 2000,
     speed: 1500,
     prevArrow: '<div class="prevArrow"><div class="arrow left"></div></div>',
     nextArrow: '<div class="nextArrow"><div class="arrow right"></div></div>',
     responsive: [
-    {
-      breakpoint: 1024,
-      settings: 'unslick'
-    }
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            variableWidth: false,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: 'unslick'
+        }
   ]
+});
+
+$( window ).resize(function() {
+    if ($(window).width() < 800) {
+        $('#workSlider').slick('unslick');
+        console.log('portrait');
+    }
+    else {
+        $('#workSlider').slick({
+            slidesToShow: 4,
+            centerMode: true,
+            variableWidth: true,
+            arrows: true,
+            swipe: true,
+            focusOnSelect: true,
+            infinite: true,
+            autoplay: false,
+            autoplaySpeed: 2000,
+            speed: 1500,
+            prevArrow: '<div class="prevArrow"><div class="arrow left"></div></div>',
+            nextArrow: '<div class="nextArrow"><div class="arrow right"></div></div>',
+            responsive: [
+                {
+                  breakpoint: 1025,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    variableWidth: false,
+                    infinite: true,
+                  }
+                },
+                {
+                  breakpoint: 800,
+                  settings: 'unslick'
+                }
+          ]
+        });
+    }
+
 });
 
 $('#displaySlider').slick({
@@ -112,6 +161,92 @@ $('#workHandle').on('click', function(){
     300);
 });
 
+
+
+
+
+$('#aboutClients .flex-module').each(function(i){
+    $(this).css({
+        'transition-delay': ((i/10)+0.1)+'s'
+    });
+});
+var horMul = 0;
+var vertFinal = 0;
+var countLoop = 0;
+var vertMul = 0;
+var moduleWidth = 490;
+var moduleHeight = 301;
+var moduleCount;
+
+if ($(window).width() < 1050) {
+   // alert('Less than 960');
+}
+else {
+   // alert('More than 960');
+   workSetup();
+}
+
+function workSetup(){
+    moduleCount = $('#workParent .work-module').length;
+    // console.log((moduleCount*moduleHeight)/2);
+    $('.work-container').css({
+        display: 'block',
+        height: ((moduleCount*moduleHeight)/2)+150
+    });
+    $('#workParent .work-module').each(function(i){
+        moduleWidth = $(this).width()+20;
+        moduleHeight = $(this).height()+20;
+        if (countLoop >= 2) {
+            vertMul++;
+            countLoop = 1;
+        }
+        else {
+            countLoop++
+        }
+        horMul = ((countLoop-1)*(moduleWidth));
+        vertFinal = (vertMul*(moduleHeight));
+        $(this).css({
+            left: horMul +'px',
+            top: vertFinal + 'px',
+            position: 'absolute'
+        });
+    });
+
+}
+
+function filterSetup(){
+    horMul = 0;
+    vertFinal = 0;
+    countLoop = 0;
+    vertMul = 0;
+    moduleCount = $('#workParent .work-module.active').length;
+    $('.work-container').css({
+        display: 'block',
+        height: ((moduleCount*moduleHeight)/2)+150
+    });
+    $('#workParent .work-module.active').each(function(i){
+        console.log(i);
+        moduleWidth = 490+20;
+        moduleHeight = 300+20;
+        if (countLoop >= 2) {
+            vertMul++;
+            countLoop = 1;
+        }
+        else {
+            countLoop++
+        }
+        horMul = ((countLoop-1)*(moduleWidth));
+        vertFinal = (vertMul*(moduleHeight));
+        $(this).css({
+            left: horMul +'px',
+            top: vertFinal + 'px',
+            position: 'absolute',
+            'transition-delay': i/20+'s'
+        });
+    });
+
+}
+
 var workModule = $('.work-module');
 var total, curData, filterData;
 
@@ -120,19 +255,36 @@ $('.filter-container > .filters').on('click', function(){
     $(this).toggleClass('active');
     filterdata = $(this).data('filter');
     total = workModule.length;
+    workModule.addClass('active');
     workModule.addClass('hide');
-
     workModule.each(function(){
-    // $(this).addClass('hide');
         curData = $(this).data('category');
         if (curData.indexOf(filterdata) > -1) {
-
             $(this).removeClass('hide');
             $(this).removeClass('hidden');
         }
+        else {
+            $(this).removeClass('active');
+        }
     });
-
     if (filterdata == 'all') {
         workModule.removeClass('hide');
+        workModule.addClass('active');
     };
+    if ($(window).width() < 1050) {
+       // alert('Less than 960');
+    }
+    else {
+       // alert('More than 960');
+       filterSetup();
+    }
+
 });
+
+$('.section-container').waypoint(function(){
+    var id = $(this.element).attr('id');
+    console.log(id);
+    $(this.element).addClass('closed');
+}, {
+  offset: '70%'
+})
